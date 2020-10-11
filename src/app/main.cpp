203c5c37b7478443da469d3a4f5bc1acac861d55
Arunkumar Mourougappane <amouroug@buffalo.edu>
2020-10-10 21:39:14 -0500

    Updated Draw API and introduction to mbed main.cpp

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,15 +1,13 @@
 #include <mbed.h>
 
-
 // Blinking rate in milliseconds
-#define BLINKING_RATE     2s
+#define BLINKING_RATE     200ms
 
 
 int main()
 {
     // Initialise the digital pin LED1 as an output
     DigitalOut led(LED1);
-
     while (true) {
         led = !led;
         ThisThread::sleep_for(BLINKING_RATE);
